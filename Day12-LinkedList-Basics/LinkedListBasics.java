ecf3007143cd334192e7e220ce054ed723e06f91
Arnav chaurasia <143399749+codearnav2221@users.noreply.github.com>
2025-09-09 11:05:27 +0530

    Update LinkedListBasics.java

--- Day12-LinkedList-Basics/LinkedListBasics.java
+++ Day12-LinkedList-Basics/LinkedListBasics.java
@@ -1,1 +1,93 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedListBasics {
+    Node head;
+
+    // Insert at head
+    public void insertAtHead(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Insert at tail
+    public void insertAtTail(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Insert at specific position (1-based index)
+    public void insertAtPosition(int data, int pos) {
+        if (pos == 1) {
+            insertAtHead(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        for (int i = 1; i < pos - 1 && temp != null; i++) {
+            temp = temp.next;
+        }
+        if (temp == null) return;
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    // Delete node by value
+    public void deleteNode(int key) {
+        if (head == null) return;
+        if (head.data == key) {
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null && temp.next.data != key) {
+            temp = temp.next;
+        }
+        if (temp.next != null) {
+            temp.next = temp.next.next;
+        }
+    }
+
+    // Print list
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        LinkedListBasics list = new LinkedListBasics();
+
+        list.insertAtHead(10);
+        list.insertAtTail(20);
+        list.insertAtTail(30);
+        list.insertAtPosition(15, 2);
+
+        System.out.print("Linked List: ");
+        list.printList();
+
+        list.deleteNode(20);
+        System.out.print("After deleting 20: ");
+        list.printList();
+    }
+}
 
